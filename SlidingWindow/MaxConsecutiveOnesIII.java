ac62a781b33ef7c72d32f9c36a7a8a0c1b3fd297
sapyyy <rijusaptadeep@gmail.com>
2026-01-16 13:17:51 +0530

    added max consecutive ones

--- SlidingWindow/MaxConsecutiveOnesIII.java
+++ SlidingWindow/MaxConsecutiveOnesIII.java
@@ -1,5 +1,25 @@
 public class MaxConsecutiveOnesIII {
-    public int longestOnes(int[] arr, int k) {
+     public int longestOnes1(int[] nums, int k) {
+        int l = 0;
+        int r = 0;
+        int n = nums.length;
+        int maxLen = 0;
+        int totalZeroes = 0;
+
+        while (r < n) {
+            if (nums[r] == 0) totalZeroes++;
+            while (totalZeroes > k) {
+                if (nums[l] == 0) totalZeroes--;
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+            r++;
+        }
+
+        return maxLen;
+    }
+    public int longestOnes2(int[] arr, int k) {
         int l = 0;
         int r = 0;
         int n = arr.length;
